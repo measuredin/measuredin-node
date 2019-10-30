fc9e982a89729ccca59d1b32a0cca6fa492766f2
Fan Jin <im.fan.jin@gmail.com>
2019-10-30 00:42:41 -0400

    Monkey patch http

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-// axios
-try {
-  require.resolve('axios');
-  const __axios = require('axios');
-  const axiosInterceptor = function(config) {
-    // TODO log request
-    return config;
-  }
-  // TODO - handle interceptor ejection
-  const defaultInterceptor = __axios.interceptors.request.use(axiosInterceptor, function (error) {
-    return Promise.reject(error);
-  });
-  const axiosFactory = __axios.create;
-  __axios.create = function (instanceConfig) {
-    const instance = axiosFactory(instanceConfig);
-    // TODO - handle interceptor ejection
-    const instanceInterceptor = instance.interceptors.request.use(axiosInterceptor, function (error) {
-      return Promise.reject(error);
-    });
-    return instance;
-  }
-} catch (error) {
-  // axios not present
+const API_HOST = 'measuredin.com'
+
+function flush(request, data) {
+
 }
+
+// http
+const http = require('http');
+const __httpRequest = http.request;
+http.request = function() {
+  const args = arguments;
+  const clientRequest = __httpRequest.apply(this, arguments);
+  const __outgoingWrite = clientRequest.write;
+  // starting v10.9.0, url can be passed in
+  // https://nodejs.org/api/http.html#http_http_request_url_options_callback
+  // const clientAgent = clientRequest.agent;
+  const reqOptions = typeof args[0] === 'string' ? args[1] : args[0];
+  // monkey patch on non-API requests
+  if (!reqOptions.hostname.includes(API_HOST)) {
+    const chunks = [];
+    clientRequest.write = function(data) {
+      chunks.push(data);
+      return __outgoingWrite.apply(this, arguments);
+    }
+    clientRequest.on('finish', function() {
+      const data = Buffer.concat(chunks).toString('utf8');
+      flush(reqOptions, data);
+    })
+  }
+  return clientRequest;
+};
