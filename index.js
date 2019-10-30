5ee23bb75512a698ff56a30607f4938b25deb0fe
Fan Jin <im.fan.jin@gmail.com>
2019-10-30 08:37:29 -0400

    Track agent and handle buffer on flush

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const API_HOST = 'measuredin.com'
 
-function flush(request, data) {
+function flush(agent, options, data) {
 
 }
 
@@ -13,7 +13,7 @@ http.request = function() {
   const __outgoingWrite = clientRequest.write;
   // starting v10.9.0, url can be passed in
   // https://nodejs.org/api/http.html#http_http_request_url_options_callback
-  // const clientAgent = clientRequest.agent;
+  const clientAgent = clientRequest.agent;
   const reqOptions = typeof args[0] === 'string' ? args[1] : args[0];
   // monkey patch on non-API requests
   if (!reqOptions.hostname.includes(API_HOST)) {
@@ -23,8 +23,8 @@ http.request = function() {
       return __outgoingWrite.apply(this, arguments);
     }
     clientRequest.on('finish', function() {
-      const data = Buffer.concat(chunks).toString('utf8');
-      flush(reqOptions, data);
+      const data = Buffer.concat(chunks);
+      flush(clientAgent, reqOptions, data);
     })
   }
   return clientRequest;
